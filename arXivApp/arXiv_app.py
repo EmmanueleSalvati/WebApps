a85f73575dbf8966a4fd943cc39e1cebe796f6d9
EmmanueleSalvati <emmanuele.salvati@gmail.com>
2015-03-05 10:33:28 -0500

    add the arxiv slider

--- arXivApp/arXiv_app.py
+++ arXivApp/arXiv_app.py
@@ -27,7 +27,7 @@ app = flask.Flask(__name__)
 def viz_page():
     """Homepage to serve the visualization page"""
 
-    with open('arXiv.html') as viz_file:
+    with open('arXiv_slider.html') as viz_file:
         return viz_file.read()
 
 
